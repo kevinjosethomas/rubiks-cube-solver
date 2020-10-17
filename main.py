81e9d16b75ec027785002503cb58abf50428bd40
TrustedMercury <kevin.jt2007@gmail.com>
2020-10-17 13:40:07 -0400

    Added display code

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
-
+import display
 
 solved_cube = [
-    [   ["white","white","white"],
-        ["white","white","white"],
-        ["white","white","white"]   ],
-    [   ["yellow","yellow","yellow"],
-        ["yellow","yellow","yellow"],
-        ["yellow","yellow","yellow"]   ],
-    [   ["blue","blue","blue"],
-        ["blue","blue","blue"],
-        ["blue","blue","blue"]   ],
-    [   ["green","green","green"],
-        ["green","green","green"],
-        ["green","green","green"]   ],
-    [   ["orange","orange","orange"],
-        ["orange","orange","orange"],
-        ["orange","orange","orange"]   ],
-    [   ["red","red","red"],
-        ["red","red","red"],
-        ["red","red","red"]   ]
+    [
+        ["w","w","w"],
+        ["w","W","w"],
+        ["w","w","w"]
+    ],
+    [
+        ["g","g","g"],
+        ["g","G","g"],
+        ["g","g","g"]
+    ],
+    [
+        ["r","r","r"],
+        ["r","R","r"],
+        ["r","r","r"]
+    ],
+    [
+        ["b","b","b"],
+        ["b","B","b"],
+        ["b","b","b"]
+    ],
+    [
+        ["o","o","o"],
+        ["o","O","o"],
+        ["o","o","o"]
+    ],
+    [
+        ["y","y","y"],
+        ["y","Y","y"],
+        ["y","y","y"]
+    ]
 ]
+
+"""
+
+       w w w
+       w W w
+       w w w
+g g g  r r r  b b b  o o o
+g G g  r R r  b B b  o O o
+g g g  r r r  b b b  o o o
+       y y y
+       y Y y
+       y y y
+
+"""
+
+display.display_cube(solved_cube)
