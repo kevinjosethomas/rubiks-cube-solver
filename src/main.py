0a323fa0146ed3283b17c1ac4904d4251934ae1d
TrustedMercury <kevin.jt2007@gmail.com>
2020-10-17 20:37:45 -0400

    Altered tests, imports and solved cubes

--- src/main.py
+++ src/main.py
@@ -1,37 +1,42 @@
+import os
+import time
+import copy
 import display
+import directions
 
 solved_cube = [
     [
         ["w","w","w"],
-        ["w","W","w"],
+        ["w","w","w"],
         ["w","w","w"]
     ],
     [
         ["g","g","g"],
-        ["g","G","g"],
+        ["g","g","g"],
         ["g","g","g"]
     ],
     [
         ["r","r","r"],
-        ["r","R","r"],
+        ["r","r","r"],
         ["r","r","r"]
     ],
     [
         ["b","b","b"],
-        ["b","B","b"],
+        ["b","b","b"],
         ["b","b","b"]
     ],
     [
         ["o","o","o"],
-        ["o","O","o"],
+        ["o","o","o"],
         ["o","o","o"]
     ],
     [
         ["y","y","y"],
-        ["y","Y","y"],
+        ["y","y","y"],
         ["y","y","y"]
     ]
 ]
+cube = copy.deepcopy(solved_cube)
 
 """
 
@@ -47,4 +52,16 @@ g g g  r r r  b b b  o o o
 
 """
 
+# Prints solved cube
 display.display_cube(solved_cube)
+
+print("\n\n")
+cube = directions.f(cube)
+display.display_cube(cube)
+print("\n\n")
+time.sleep(1)
+
+cube = directions.f_(cube)
+display.display_cube(cube)
+print("\n")
+time.sleep(1)
