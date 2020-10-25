69a7169f70e494658a767987790c260d9d00a641
TrustedMercury <kevin.jt2007@gmail.com>
2020-10-25 17:14:06 -0400

    Added all cube directional rotations

--- src/directions.py
+++ src/directions.py
@@ -24,16 +24,18 @@ orange - 4
 yellow = 5
 """
 
+# Clockwise Rotations
+
 def u(cube):
     temp_cube = copy.deepcopy(cube)
 
-    # Front Face
+    # Changing same color
     temp_cube[1][0] = cube[2][0]
     temp_cube[2][0] = cube[3][0]
     temp_cube[3][0] = cube[4][0]
     temp_cube[4][0] = cube[1][0]
 
-    # Top Face
+    # Changing around
     temp_cube[0][2][0] = cube[0][2][2]
     temp_cube[0][2][1] = cube[0][1][2]
     temp_cube[0][2][2] = cube[0][0][2]
@@ -49,62 +51,157 @@ def u(cube):
 
     return temp_cube
 
-def u_(cube):
+def l(cube):
     temp_cube = copy.deepcopy(cube)
 
-    # Front Face
-    temp_cube[2][0] = cube[1][0]
-    temp_cube[3][0] = cube[2][0]
-    temp_cube[4][0] = cube[3][0]
-    temp_cube[1][0] = cube[4][0]
+    # Changing same color
+    temp_cube[1][0][0] = cube[1][2][0]
+    temp_cube[1][0][1] = cube[1][1][0]
+    temp_cube[1][0][2] = cube[1][0][0]
 
-    # Top Face
-    temp_cube[0][2][2] = cube[0][2][0]
-    temp_cube[0][1][2] = cube[0][2][1]
-    temp_cube[0][0][2] = cube[0][2][2]
+    temp_cube[1][1][2] = cube[1][0][1]
+    temp_cube[1][2][2] = cube[1][0][2]
 
-    temp_cube[0][0][1] = cube[0][1][2]
-    temp_cube[0][0][0] = cube[0][0][2]
+    temp_cube[1][2][1] = cube[1][1][2]
+    temp_cube[1][2][0] = cube[1][2][2]
 
-    temp_cube[0][1][0] = cube[0][0][1]
-    temp_cube[0][2][2] = cube[0][2][0]
+    temp_cube[1][1][0] = cube[1][2][1]
 
-    temp_cube[0][2][0] = cube[0][0][0]
-    temp_cube[0][2][1] = cube[0][1][0]
+    # Changing around
+    temp_cube[5][0][0] = cube[2][0][0]
+    temp_cube[5][1][0] = cube[2][1][0]
+    temp_cube[5][2][0] = cube[2][2][0]
+
+    temp_cube[4][2][2] = cube[5][0][0]
+    temp_cube[4][1][2] = cube[5][1][0]
+    temp_cube[4][0][2] = cube[5][2][0]
+
+    temp_cube[0][0][0] = cube[4][2][2]
+    temp_cube[0][1][0] = cube[4][1][2]
+    temp_cube[0][2][0] = cube[4][0][2]
+
+    temp_cube[2][0][0] = cube[0][0][0]
+    temp_cube[2][1][0] = cube[0][1][0]
+    temp_cube[2][2][0] = cube[0][2][0]
+
+    return temp_cube
+
+def f(cube):
+    temp_cube = copy.deepcopy(cube)
+
+    # Changing same color
+    temp_cube[2][0][2] = cube[2][0][0]
+    temp_cube[2][1][2] = cube[2][0][1]
+    temp_cube[2][2][2] = cube[2][0][2]
+
+    temp_cube[2][2][1] = cube[2][1][2]
+    temp_cube[2][2][0] = cube[2][2][2]
+
+    temp_cube[2][1][0] = cube[2][2][1]
+    temp_cube[2][0][0] = cube[2][2][0]
+
+    temp_cube[2][0][1] = cube[2][1][0]
+
+
+    # Changing around
+    temp_cube[3][0][0] = cube[0][2][0]
+    temp_cube[3][1][0] = cube[0][2][1]
+    temp_cube[3][2][0] = cube[0][2][2]
+
+    temp_cube[5][0][2] = cube[3][0][0]
+    temp_cube[5][0][1] = cube[3][1][0]
+    temp_cube[5][0][0] = cube[3][2][0]
+
+    temp_cube[1][0][2] = cube[5][0][0]
+    temp_cube[1][1][2] = cube[5][0][1]
+    temp_cube[1][2][2] = cube[5][0][2]
+
+    temp_cube[0][2][2] = cube[1][0][2]
+    temp_cube[0][2][1] = cube[1][1][2]
+    temp_cube[0][2][0] = cube[1][2][2]
+
+    return temp_cube
+
+def r(cube):
+    temp_cube = copy.deepcopy(cube)
+
+    # Changing same color
+    temp_cube[3][0][0] = cube[3][2][0]
+    temp_cube[3][0][1] = cube[3][1][0]
+    temp_cube[3][0][2] = cube[3][0][0]
+
+    temp_cube[3][1][2] = cube[3][0][1]
+    temp_cube[3][2][2] = cube[3][0][2]
+
+    temp_cube[3][2][1] = cube[3][1][2]
+    temp_cube[3][2][0] = cube[3][2][2]
+
+    temp_cube[3][1][0] = cube[3][2][1]
+
+    # Changing around
+    temp_cube[2][0][2] = cube[5][0][2]
+    temp_cube[2][1][2] = cube[5][1][2]
+    temp_cube[2][2][2] = cube[5][2][2]
+
+    temp_cube[5][0][2] = cube[4][2][0]
+    temp_cube[5][1][2] = cube[4][1][0]
+    temp_cube[5][2][2] = cube[4][0][0]
+
+    temp_cube[4][2][0] = cube[0][0][2]
+    temp_cube[4][1][0] = cube[0][1][2]
+    temp_cube[4][0][0] = cube[0][2][2]
+
+    temp_cube[0][0][2] = cube[2][0][2]
+    temp_cube[0][1][2] = cube[2][1][2]
+    temp_cube[0][2][2] = cube[2][2][2]
+
+    return temp_cube
+
+def b(cube):
+    temp_cube = copy.deepcopy(cube)
+
+    # Changing same color
+    temp_cube[4][0][2] = cube[4][0][0]
+    temp_cube[4][1][2] = cube[4][0][1]
+    temp_cube[4][2][2] = cube[4][0][2]
+
+    temp_cube[4][2][1] = cube[4][1][2]
+    temp_cube[4][2][0] = cube[4][2][2]
+
+    temp_cube[4][1][0] = cube[4][2][1]
+    temp_cube[4][0][0] = cube[4][2][0]
+
+    temp_cube[4][0][1] = cube[4][1][0]
+
+    # Changing around
+    temp_cube[0][0][0] = cube[3][0][2]
+    temp_cube[0][0][1] = cube[3][1][2]
+    temp_cube[0][0][2] = cube[3][2][2]
+
+    temp_cube[3][0][2] = cube[5][2][2]
+    temp_cube[3][1][2] = cube[5][2][1]
+    temp_cube[3][2][2] = cube[5][2][0]
+
+    temp_cube[5][2][0] = cube[1][0][0]
+    temp_cube[5][2][1] = cube[1][1][0]
+    temp_cube[5][2][2] = cube[1][2][0]
+
+    temp_cube[1][0][0] = cube[0][0][2]
+    temp_cube[1][1][0] = cube[0][0][1]
+    temp_cube[1][2][0] = cube[0][0][0]
 
     return temp_cube
 
 def d(cube):
     temp_cube = copy.deepcopy(cube)
-    temp_cube[1][2] = cube[2][2]
-    temp_cube[2][2] = cube[3][2]
-    temp_cube[3][2] = cube[4][2]
-    temp_cube[4][2] = cube[1][2]
 
-    # Top Face
-    temp_cube[5][2][2] = cube[5][2][0]
-    temp_cube[5][1][2] = cube[5][2][1]
-    temp_cube[5][0][2] = cube[5][2][2]
-
-    temp_cube[5][0][1] = cube[5][1][2]
-    temp_cube[5][0][0] = cube[5][0][2]
-
-    temp_cube[5][1][0] = cube[5][0][1]
-    temp_cube[5][2][2] = cube[5][2][0]
-
-    temp_cube[5][2][0] = cube[5][0][0]
-    temp_cube[5][2][1] = cube[5][1][0]
-
-    return temp_cube
-
-def d_(cube):
-    temp_cube = copy.deepcopy(cube)
+    # Changing same color
     temp_cube[2][2] = cube[1][2]
     temp_cube[3][2] = cube[2][2]
     temp_cube[4][2] = cube[3][2]
     temp_cube[1][2] = cube[4][2]
 
-    # Top Face
+    # Changing around
     temp_cube[5][2][0] = cube[5][2][2]
     temp_cube[5][2][1] = cube[5][1][2]
     temp_cube[5][2][2] = cube[5][0][2]
@@ -120,91 +217,250 @@ def d_(cube):
 
     return temp_cube
 
-def f(cube):
+
+# Counterclockwise Rotations
+
+def u_(cube):
     temp_cube = copy.deepcopy(cube)
 
-    # Top Edge
-    temp_cube[2][0][2] = cube[2][0][0]
-    temp_cube[2][1][2] = cube[2][0][1]
-    temp_cube[2][2][2] = cube[2][0][2]
+    # Changing same color
+    temp_cube[2][0] = cube[1][0]
+    temp_cube[3][0] = cube[2][0]
+    temp_cube[4][0] = cube[3][0]
+    temp_cube[1][0] = cube[4][0]
 
-    # Right Edge
-    temp_cube[2][2][1] = cube[2][1][2]
-    temp_cube[2][2][0] = cube[2][2][2]
+    # Changing around
+    temp_cube[0][2][2] = cube[0][2][0]
+    temp_cube[0][1][2] = cube[0][2][1]
+    temp_cube[0][0][2] = cube[0][2][2]
 
-    # Bottom Edge
-    temp_cube[2][1][0] = cube[2][2][1]
-    temp_cube[2][0][0] = cube[2][2][0]
+    temp_cube[0][0][1] = cube[0][1][2]
+    temp_cube[0][0][0] = cube[0][0][2]
 
-    # Left Edge
-    temp_cube[2][0][1] = cube[2][1][0]
+    temp_cube[0][1][0] = cube[0][0][1]
+    temp_cube[0][2][2] = cube[0][2][0]
 
+    temp_cube[0][2][0] = cube[0][0][0]
+    temp_cube[0][2][1] = cube[0][1][0]
 
-    # Right -> Top
-    temp_cube[3][0][0] = cube[0][2][0]
-    temp_cube[3][1][0] = cube[0][2][1]
-    temp_cube[3][2][0] = cube[0][2][2]
+    return temp_cube
 
-    # Bottom -> Right
-    temp_cube[5][0][2] = cube[3][0][0]
-    temp_cube[5][0][1] = cube[3][1][0]
-    temp_cube[5][0][0] = cube[3][2][0]
+def l_(cube):
+    temp_cube = copy.deepcopy(cube)
 
-    # Left -> Bottom
-    temp_cube[1][0][2] = cube[5][0][0]
-    temp_cube[1][1][2] = cube[5][0][1]
-    temp_cube[1][2][2] = cube[5][0][2]
+    # Changing same color
+    temp_cube[1][2][0] = cube[1][0][0]
+    temp_cube[1][1][0] = cube[1][0][1]
+    temp_cube[1][0][0] = cube[1][0][2]
 
-    # Top -> Left
-    temp_cube[0][2][2] = cube[1][0][2]
-    temp_cube[0][2][1] = cube[1][1][2]
-    temp_cube[0][2][0] = cube[1][2][2]
+    temp_cube[1][0][1] = cube[1][1][2]
+    temp_cube[1][0][2] = cube[1][2][2]
+
+    temp_cube[1][1][2] = cube[1][2][1]
+    temp_cube[1][2][2] = cube[1][2][0]
+
+    temp_cube[1][2][1] = cube[1][1][0]
+
+    # Changing around
+    temp_cube[2][0][0] = cube[5][0][0]
+    temp_cube[2][1][0] = cube[5][1][0]
+    temp_cube[2][2][0] = cube[5][2][0]
+
+    temp_cube[5][0][0] = cube[4][2][2]
+    temp_cube[5][1][0] = cube[4][1][2]
+    temp_cube[5][2][0] = cube[4][0][2]
+
+    temp_cube[4][2][2] = cube[0][0][0]
+    temp_cube[4][1][2] = cube[0][1][0]
+    temp_cube[4][0][2] = cube[0][2][0]
+
+    temp_cube[0][0][0] = cube[2][0][0]
+    temp_cube[0][1][0] = cube[2][1][0]
+    temp_cube[0][2][0] = cube[2][2][0]
 
     return temp_cube
 
 def f_(cube):
     temp_cube = copy.deepcopy(cube)
 
-    # Top Edge
+    # Changing same color
     temp_cube[2][0][0] = cube[2][0][2]
     temp_cube[2][0][1] = cube[2][1][2]
     temp_cube[2][0][2] = cube[2][2][2]
 
-    # Right Edge
     temp_cube[2][1][2] = cube[2][2][1]
     temp_cube[2][2][2] = cube[2][2][0]
 
-    # Bottom Edge
     temp_cube[2][2][1] = cube[2][1][0]
     temp_cube[2][2][0] = cube[2][0][0]
 
-    # Left Edge
     temp_cube[2][1][0] = cube[2][0][1]
 
-
-    # Right -> Top
+    # Changing around
     temp_cube[0][2][0] = cube[3][0][0]
     temp_cube[0][2][1] = cube[3][1][0]
     temp_cube[0][2][2] = cube[3][2][0]
 
-    # Bottom -> Right
     temp_cube[3][0][0] = cube[5][0][2]
     temp_cube[3][1][0] = cube[5][0][1]
     temp_cube[3][2][0] = cube[5][0][0]
 
-    # Left -> Bottom
     temp_cube[5][0][0] = cube[1][0][2]
     temp_cube[5][0][1] = cube[1][1][2]
     temp_cube[5][0][2] = cube[1][2][2]
 
-    # Top -> Left
     temp_cube[1][0][2] = cube[0][2][2]
     temp_cube[1][1][2] = cube[0][2][1]
     temp_cube[1][2][2] = cube[0][2][0]
 
     return temp_cube
 
+def r_(cube):
+    temp_cube = copy.deepcopy(cube)
 
+    # Changing same color
+    temp_cube[3][2][0] = cube[3][0][0]
+    temp_cube[3][1][0] = cube[3][0][1]
+    temp_cube[3][0][0] = cube[3][0][2]
+
+    temp_cube[3][0][1] = cube[3][1][2]
+    temp_cube[3][0][2] = cube[3][2][2]
+
+    temp_cube[3][1][2] = cube[3][2][1]
+    temp_cube[3][2][2] = cube[3][2][0]
+
+    temp_cube[3][2][1] = cube[3][1][0]
+
+    # Changing around
+    temp_cube[5][0][2] = cube[2][0][2]
+    temp_cube[5][1][2] = cube[2][1][2]
+    temp_cube[5][2][2] = cube[2][2][2]
+
+    temp_cube[4][2][0] = cube[5][0][2]
+    temp_cube[4][1][0] = cube[5][1][2]
+    temp_cube[4][0][0] = cube[5][2][2]
+
+    temp_cube[0][0][2] = cube[4][2][0]
+    temp_cube[0][1][2] = cube[4][1][0]
+    temp_cube[0][2][2] = cube[4][0][0]
+
+    temp_cube[2][0][2] = cube[0][0][2]
+    temp_cube[2][1][2] = cube[0][1][2]
+    temp_cube[2][2][2] = cube[0][2][2]
+
+    return temp_cube
+
+def b_(cube):
+    temp_cube = copy.deepcopy(cube)
+
+    # Changing same color
+    temp_cube[4][0][0] = cube[4][0][2]
+    temp_cube[4][0][1] = cube[4][1][2]
+    temp_cube[4][0][2] = cube[4][2][2]
+
+    temp_cube[4][1][2] = cube[4][2][1]
+    temp_cube[4][2][2] = cube[4][2][0]
+
+    temp_cube[4][2][1] = cube[4][1][0]
+    temp_cube[4][2][0] = cube[4][0][0]
+
+    temp_cube[4][1][0] = cube[4][0][1]
+
+
+    # Changing around
+    temp_cube[3][0][2] = cube[0][0][0]
+    temp_cube[3][1][2] = cube[0][0][1]
+    temp_cube[3][2][2] = cube[0][0][2]
+
+    temp_cube[5][2][2] = cube[3][0][2]
+    temp_cube[5][2][1] = cube[3][1][2]
+    temp_cube[5][2][0] = cube[3][2][2]
+
+    temp_cube[1][0][0] = cube[5][2][0]
+    temp_cube[1][1][0] = cube[5][2][1]
+    temp_cube[1][2][0] = cube[5][2][2]
+
+    temp_cube[0][0][2] = cube[1][0][0]
+    temp_cube[0][0][1] = cube[1][1][0]
+    temp_cube[0][0][0] = cube[1][2][0]
+
+    return temp_cube
+
+def d_(cube):
+    temp_cube = copy.deepcopy(cube)
+
+    # Changing same color
+    temp_cube[1][2] = cube[2][2]
+    temp_cube[2][2] = cube[3][2]
+    temp_cube[3][2] = cube[4][2]
+    temp_cube[4][2] = cube[1][2]
+
+    # Changing around
+    temp_cube[5][2][2] = cube[5][2][0]
+    temp_cube[5][1][2] = cube[5][2][1]
+    temp_cube[5][0][2] = cube[5][2][2]
+
+    temp_cube[5][0][1] = cube[5][1][2]
+    temp_cube[5][0][0] = cube[5][0][2]
+
+    temp_cube[5][1][0] = cube[5][0][1]
+    temp_cube[5][2][2] = cube[5][2][0]
+
+    temp_cube[5][2][0] = cube[5][0][0]
+    temp_cube[5][2][1] = cube[5][1][0]
+
+    return temp_cube
+
+
+# Slice Turns
+
+def m(cube):
+    temp_cube = copy.deepcopy(cube)
+
+    temp_cube = r(temp_cube)
+    temp_cube = l_(temp_cube)
+
+    return temp_cube
+
+def m_(cube):
+    temp_cube = copy.deepcopy(cube)
+
+    temp_cube = r_(temp_cube)
+    temp_cube = l(temp_cube)
+
+    return temp_cube
+
+def e(cube):
+    temp_cube = copy.deepcopy(cube)
+
+    temp_cube = u(temp_cube)
+    temp_cube = d_(temp_cube)
+
+    return temp_cube
+
+def e_(cube):
+    temp_cube = copy.deepcopy(cube)
+
+    temp_cube = u_(temp_cube)
+    temp_cube = d(temp_cube)
+
+    return temp_cube
+
+def s(cube):
+    temp_cube = copy.deepcopy(cube)
+
+    temp_cube = f_(temp_cube)
+    temp_cube = b(temp_cube)
+
+    return temp_cube
+
+def s_(cube):
+    temp_cube = copy.deepcopy(cube)
+
+    temp_cube = f(temp_cube)
+    temp_cube = b_(temp_cube)
+
+    return temp_cube
 
 """
 white - 0
@@ -212,5 +468,5 @@ green - 1
 red - 2
 blue - 3
 orange - 4
-yellow = 5
+yellow - 5
 """
